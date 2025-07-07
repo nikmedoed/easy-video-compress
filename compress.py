32c83da0aad12c21385f339d8782143c8d16e893
Nikita Muromtsev <grafniksan@gmail.com>
2025-07-07 21:41:48 +0400

    Improve row progress and alignment in Qt UI

--- compress.py
+++ compress.py
@@ -234,6 +234,21 @@ class DropTable(QtWidgets.QTableWidget):
         event.acceptProposedAction()
 
 
+class ProgressDelegate(QtWidgets.QStyledItemDelegate):
+    def __init__(self, progress, *args):
+        super().__init__(*args)
+        self.progress = progress
+
+    def paint(self, painter, option, index):
+        pr = self.progress.get(index.row(), 0)
+        if pr:
+            bar_rect = QtCore.QRect(option.rect)
+            bar_rect.setWidth(int(option.rect.width() * pr / 100))
+            color = option.palette.highlight().color().lighter(130)
+            painter.fillRect(bar_rect, color)
+        super().paint(painter, option, index)
+
+
 def run_gui():
     app = QtWidgets.QApplication(sys.argv)
     win = QtWidgets.QWidget()
@@ -273,10 +288,9 @@ def run_gui():
         "result",
         "five_mb",
         "alt",
-        "progress",
     ]
     table = DropTable(len(columns))
-    table.setHorizontalHeaderLabels(["File", "Codec", "Bitrate", "Duration", "Size", "Result", "5MB?", "Alt", "Progress"])
+    table.setHorizontalHeaderLabels(["File", "Codec", "Bitrate", "Duration", "Size", "Result", "5MB?", "Alt"])
     header = table.horizontalHeader()
     header.setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
     header.setStretchLastSection(True)
@@ -295,7 +309,9 @@ def run_gui():
     scroll_bar.valueChanged.connect(on_scroll)
 
     info = {}
-    progress_bars = {}
+    progress_map = {}
+    delegate = ProgressDelegate(progress_map, table)
+    table.setItemDelegate(delegate)
     alt_buttons = {}
     executor = ThreadPoolExecutor(max_workers=MAX_WORKERS)
     total = 0
@@ -315,7 +331,7 @@ def run_gui():
             return
         for row in range(table.rowCount()):
             inf = info.get(row)
-            if inf and not inf.get("done") and progress_bars[row].value() < 100:
+            if inf and not inf.get("done") and inf.get("progress", 0) < 100:
                 table.scrollToItem(table.item(row, 0), QtWidgets.QAbstractItemView.PositionAtTop)
                 break
 
@@ -327,18 +343,23 @@ def run_gui():
             except queue.Empty:
                 break
             if evt == "progress":
-                progress_bars[row].setValue(int(data))
+                progress_map[row] = data
+                info[row]["progress"] = data
             elif evt == "done":
-                progress_bars[row].setValue(100)
+                progress_map[row] = 100
                 table.item(row, 5).setText(f"{data:.1f} MB")
                 info[row]["done"] = True
+                info[row]["progress"] = 100
                 done += 1
                 update_overall()
             elif evt == "error":
+                progress_map[row] = 100
                 table.item(row, 5).setText("error")
                 info[row]["done"] = True
+                info[row]["progress"] = 100
                 done += 1
                 update_overall()
+        table.viewport().update()
         scroll_to_current()
 
     timer = QtCore.QTimer()
@@ -356,25 +377,37 @@ def run_gui():
             mode = mode_override or ("size" if size_box.isChecked() else "crf")
             row = table.rowCount()
             table.insertRow(row)
-            table.setItem(row, 0, QtWidgets.QTableWidgetItem(path.name))
-            table.setItem(row, 1, QtWidgets.QTableWidgetItem(codec))
-            table.setItem(row, 2, QtWidgets.QTableWidgetItem(f"{br//1000}k"))
-            table.setItem(row, 3, QtWidgets.QTableWidgetItem(format_duration(dur)))
-            table.setItem(row, 4, QtWidgets.QTableWidgetItem(f"{size_mb:.1f} MB"))
-            table.setItem(row, 5, QtWidgets.QTableWidgetItem(""))
-            table.setItem(row, 6, QtWidgets.QTableWidgetItem("✔" if mode == "size" else ""))
+            item_file = QtWidgets.QTableWidgetItem(path.name)
+            table.setItem(row, 0, item_file)
+            item_codec = QtWidgets.QTableWidgetItem(codec)
+            item_codec.setTextAlignment(QtCore.Qt.AlignCenter)
+            table.setItem(row, 1, item_codec)
+            item_br = QtWidgets.QTableWidgetItem(f"{br//1000}k")
+            item_br.setTextAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+            table.setItem(row, 2, item_br)
+            item_dur = QtWidgets.QTableWidgetItem(format_duration(dur))
+            item_dur.setTextAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+            table.setItem(row, 3, item_dur)
+            item_size = QtWidgets.QTableWidgetItem(f"{size_mb:.1f} MB")
+            item_size.setTextAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+            table.setItem(row, 4, item_size)
+            item_res = QtWidgets.QTableWidgetItem("")
+            item_res.setTextAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
+            table.setItem(row, 5, item_res)
+            item_mode = QtWidgets.QTableWidgetItem("✔" if mode == "size" else "")
+            item_mode.setTextAlignment(QtCore.Qt.AlignCenter)
+            table.setItem(row, 6, item_mode)
 
             btn_alt = QtWidgets.QPushButton("⇆")
             btn_alt.setMaximumWidth(40)
+            btn_alt.setStyleSheet("padding:0px;margin:0px;border:none;")
+            btn_alt.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
             btn_alt.clicked.connect(lambda _, p=str(path), m="crf" if mode == "size" else "size": add_files([p], m))
             table.setCellWidget(row, 7, btn_alt)
 
-            pb = QtWidgets.QProgressBar()
-            pb.setValue(0)
-            table.setCellWidget(row, 8, pb)
-            progress_bars[row] = pb
+            progress_map[row] = 0
             alt_buttons[row] = btn_alt
-            info[row] = {"path": path, "duration": dur, "mode": mode, "done": False}
+            info[row] = {"path": path, "duration": dur, "mode": mode, "done": False, "progress": 0}
             auto_scroll = True
             scroll_to_current()
             total += 1
